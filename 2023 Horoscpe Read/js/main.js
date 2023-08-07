57a5527ea27598a2d52fcc956a74bbe3036a4dd1
noebuilds <37415634+noebuilds@users.noreply.github.com>
2023-08-06 19:27:34 -0700

    Add files via upload

---
+++ 2023 Horoscpe Read/js/main.js
@@ -0,0 +1,50 @@
+document.getElementById ('Aries').onclick = ariesMsg 
+document.getElementById ('Taurus').onclick = TaurusMsg
+document.getElementById ('Gemini').onclick = GeminiMsg 
+document.getElementById ('Cancer').onclick = CancerMsg 
+document.getElementById ('Leo').onclick = LeoMsg 
+document.getElementById ('Virgo').onclick = VirgoMsg 
+document.getElementById ('Libra').onclick = LibraMsg 
+document.getElementById ('Scorpio').onclick = ScorpioMsg 
+document.getElementById ('Sagittarius').onclick = SagittariusMsg 
+document.getElementById ('Capricorn').onclick = CapricornMsg 
+document.getElementById ('Aquarius').onclick = AquariusMsg 
+document.getElementById ('Pisces').onclick = PiscesMsg 
+
+
+function ariesMsg () {
+    document.getElementById('title').innerText = 'Luck be a horned creature in the first part of 2023 as blessing bestowing drunk uncle planet Jupiter is riding high in your sign until mid May. Jupiter gives you a mountain top perspective on your own life and encourages you to dream the dream, Aries. All things conceivable are possible for you now. The focus for you this year is not on winning but on your own wanting. As you well know, you burn bright but often burn out. Combat this consequence by making a plan, biding your time, knowing yourself and believing that you can get further by going slower.'
+}
+function TaurusMsg () {
+    document.getElementById('title').innerText = 'A season of eclipses in your sign last year Taurus asked you to redefine your sense of self and the relationship you have with desire and abundance and your ability to confidently claim both. Jupiter, planet of golden light and endless expansion moves into your sign at the tail end of Taurus season. Jupiter wants to give you all that you need and more of what you want. The trick to receiving my bull babies, is convincing yourself that you deserve it. Whispers lovingly: you do.'
+}
+function GeminiMsg () {
+    document.getElementById('title').innerText = 'Gemini folk, your year begins with Mars retrograding in your sign. As a mover and shaker, you prefer to live life at the rate of exchange but this backspin demands that you take stock of where, how and to whom you invest your prodigious energies. Your gift is your ability to adapt to changing times and shape shift when social situations call for it but the intention of this retrograde, which sets the tone for your year ahead, is to harness those talents in a practical, and dare I say profitable way. You are more than a court jester with a quick wit Gemini, you are Hermes incarnate, capable of distilling information, influencing others and swinging wildly from high end to low brow, upper echelon to under belly. For the sake of all of us, make the most of your musedom.'
+}
+function CancerMsg () {
+    document.getElementById('title').innerText ='Pluto, planet of sex, death, regeneration and fishnet stockings is setting up shop in your eighth house of intimacy where it will eventually kick up its black heels for the next two decades. Pluto welcomes you to dive deep into yourself and your closest connections. You have a propensity to give yourself fully and freely to your nearest and dearest, believing that what you give equates to what you are worth. No more of that s**t, Cancer. Think of this year as an oxygen mask, get what you need to survive before aiding or accommodating others. Take care of you and give only and ever when you have enough and believe you are enough.'
+}
+function LeoMsg () {
+    document.getElementById('title').innerText ='have a PSA for you Leo: you exist to shine and the frequency of that shine is unrelated to its reception. You do not need the validation of others to show you that you are special. You were born with the sun on your side and a song in your heart. Do not dial down the volume to make others more comfortable in their own beige complacency. Neigh. Roar and revel, not for the cheap thrill of attention but to alert others to the deep satisfaction that can be found in being your fullest, least apologetic self'
+}
+function VirgoMsg () {
+    document.getElementById('title').innerText ='The journey is the destination Virgo. Your greatest power is your ability to see what can be, a kind of prophecy for potential that is most pointed in regard to your own becoming. Often, you fall short of fulfilling what you hope to be but only because you forget to be patient with the process. The live birth of your true self is a life long gestation and everything you endure and every skin you shed is bringing you closer to it. Trust that all good things take shape in good time, and you, my hallowed Virgo, are a very, very good thing.'
+}
+function LibraMsg () {
+    document.getElementById('title').innerText ='Ahh Libra, you are so easy to love and yet so hard to truly know. You have a natural ability to sense what others need or expect from you. Yet, this ability to sense the needs of others does not translate to an obligation to fulfill them. Indeed, yours is a sign that often lets people pleasing take precedence over self-assessment. But guess what? We can and should overcome our impulses, and in the case of you this year Libra, that means getting good and clear and ruthlessly selfish about what you want. It’s a tall order but a necessary one. There is no limit to what you can be when you set yourself free from satisfying others.'
+}
+function ScorpioMsg () {
+    document.getElementById('title').innerText ='A mean girl I once loved told me that a Scorpio is just a Cancer in a black leather jacket. It stings of truth as beneath your veneer of toughness and your well constructed ramparts of self preservation beats a very, very tender animal. The transits of 2022 and the first half of 2023 call you to question what and who you are protecting yourself from. Are you operating from a place of love or fear, Scorpio? As a sign that understands control more than others, you often need a lesson in letting go and letting in. Thank the guardians of your heart meat for what they have saved you from in the past and ask them to ease up as you walk boldly (or tentatively, you do you) towards the rich, anxious landscape of intimacy.'
+}
+function SagittariusMsg () {
+    document.getElementById('title').innerText ='Yours is the sign of the seeker, Sagittarius and this year you are called to share what you’ve found and to suss out sages and mentors who possess the wisdom that remains. The word mentor comes from Homer’s “The Odyssey,” Mentor being the gray hair that Odysseus charged with educating and guiding his young son, Telemachus. The Mentor of olde low key failed at his task and was later replaced by Athena, goddess of wisdom who shape shifted into his aged form to instruct Telemachus on how to defend his father’s kingdom. The lesson here, and the one that archers should heed in 2023, is that age does not guarantee wisdom and it takes many hands and more than a few philosophies to set a person on their path.'
+}
+function CapricornMsg () {
+    document.getElementById('title').innerText ='Lighten up, brighten up sea goats. This year, your ruling planet, structured dad jeans Saturn is moving out of Aquarius and into dream weaving Pisces. And though whimsy has never really been your bag, Capricorn, this transition speaks to your own dreams being given the freedom to form with the structure to flourish. We love to see it, folks. This energy is supported by Pluto, planet of death and power struggles, moving out of your sign for the first time in FIFTEEN YEARS, signaling a rise out of the underworld and away from the shadows. Onward and upward into the sunshine and the mountain top, Cap. You lead the way.'
+}
+function AquariusMsg () {
+    document.getElementById('title').innerText ='Metamorphosis and decent into the shadow realm of the self is at the forefront for you in the year ahead, Aquarius as Pluto, planet of death and transformation takes up residence in your sign from March 23-June 11. This transit is but a preview of what power and potency Pluto has in store when it stations in Aquarius next January, where it will remain uninterrupted for the next twenty years. The last time Pluto moved through revolutionary Aquarius was 1778-1798, some pretty heady years for change and upheaval if history serves. For water bearers this Pluto transit calls you to question how you conceive of yourself as both an individual and a member of the collective. You are both humanitarian and hermit, compassionate and calculating. Your singular mind can often make you feel like an outsider but is that identification your way of keeping your distance? A loner laurel to rest on? The world needs the lens you can offer it. Don’t hide what you were born to share.'
+}
+function PiscesMsg () {
+    document.getElementById('title').innerText = 'Dad planet Saturn is moving in and staking claims in your sign, Pisces. The parental planet will spend three years lending structure and building boundaries in and around your watery self and while structure sounds like a bore it has its benefits and you are rife to reap them. In that realm of reaping, Saturn is a tough planet but one that rewards work and forethought, this year and this transit just might see you come to full bloom after many years of planting your purpose. If you work with, rather than against the powers of Saturn you are more primed than ever to define yourself on your own terms, independent of the brackish backsplash of others needs, wants, projections and psychic detritus. Solid ground and the firm footing of selfhood looks great on you.'
+}
